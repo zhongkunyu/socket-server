14419b6949cb7768a3efafac7d13110031b2ea5c
Sam Zhong <zhongkunyu@gmail.com>
2017-11-23 16:59:58 +0800

    增加接收client发送的数据的处理

--- test.c
+++ test.c
@@ -6,6 +6,39 @@
 #include <unistd.h>
 #include <signal.h>
 
+static void* 
+client_event_proc(void *arg)
+{
+    int new_client = (int)arg;
+    char buf[1024];
+
+    printf("create a new thread, thread_id is: %u\n", pthread_self());
+
+    memset(buf, '\0', sizeof(buf));
+    while (1)
+    {
+        //reading from new_client and store to buf
+        ssize_t  _size = read(new_client, buf, sizeof(buf) - 1);
+        if (_size < 0)
+        {
+            perror("read");
+            break;
+        }
+        else if (_size == 0)
+        {
+            printf("client release!\n");
+            break;
+        }
+        else
+        {
+            buf[_size] = '\0';
+            printf("client:-> %s", buf);
+        }
+    }
+    close(new_client);
+}
+
+
 static void *
 _poll(void * ud) {
 	struct socket_server *ss = ud;
@@ -31,6 +64,12 @@ _poll(void * ud) {
 			break;
 		case SOCKET_ACCEPT:
 			printf("accept(%lu) [id=%d %s] from [%d]\n",result.opaque, result.ud, result.data, result.id);
+            {
+                pthread_t pid;
+                pthread_create(&pid, NULL, client_event_proc, result.opaque);
+
+                pthread_join(pid, NULL);
+            }
 			break;
 		}
 	}
@@ -38,24 +77,25 @@ _poll(void * ud) {
 
 static void
 test(struct socket_server *ss) {
-	pthread_t pid;
-	pthread_create(&pid, NULL, _poll, ss);
+	//pthread_t pid;
+	//pthread_create(&pid, NULL, _poll, ss);
 
-	int c = socket_server_connect(ss,100,"127.0.0.1",80);
-	printf("connecting %d\n",c);
-	int l = socket_server_listen(ss,200,"127.0.0.1",8888,32);
+	//int c = socket_server_connect(ss,100,"127.0.0.1",80);
+	//printf("connecting %d\n",c);
+	int l = socket_server_listen(ss,200,"0.0.0.0",8888,32);
 	printf("listening %d\n",l);
 	socket_server_start(ss,201,l);
-	int b = socket_server_bind(ss,300,1);
-	printf("binding stdin %d\n",b);
-	int i;
-	for (i=0;i<100;i++) {
-		socket_server_connect(ss, 400+i, "127.0.0.1", 8888);
-	}
-	sleep(5);
-	socket_server_exit(ss);
+	//int b = socket_server_bind(ss,300,1);
+	//printf("binding stdin %d\n",b);
+	//int i;
+	//for (i=0;i<100;i++) {
+	//	socket_server_connect(ss, 400+i, "127.0.0.1", 8888);
+	//}
+	//sleep(5);
+	//socket_server_exit(ss);
+    _poll(ss);
 
-	pthread_join(pid, NULL); 
+	//pthread_join(pid, NULL); 
 }
 
 int
